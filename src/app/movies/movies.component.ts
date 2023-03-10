a61928d10116acc19069eb8f20c098f80037c74f
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-03-10 11:43:45 +0100

    :sparkles: feat: create search input

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -8,7 +8,8 @@ import { MoviesService } from "./movies.service";
     standalone: true,
     selector: "tp-movies-movies-api",
     templateUrl: "./movies.component.html",
-    styleUrls: [],
+    styleUrls: ['./movies.component.scss'],
+
     imports: [CommonModule, HeaderComponent, FooterComponent]
 })
 export class MoviesComponent {
