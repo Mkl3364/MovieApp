300d17ddbdf4af20612a6d20a4d071ba77fc2cc3
Mickael Brunet <mickael.brunet64@gmail.com>
2023-03-10 15:14:54 +0100

    Add footer component to profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
+import { FooterComponent } from "../footer/footer.component";
 
 
 @Component({
     standalone: true,
     selector: 'tp-movies-profile',
     templateUrl: 'profile.component.html',
-    styleUrls: ['./profile.component.scss']
+    styleUrls: ['./profile.component.scss'],
+    imports: [FooterComponent]
 })
 export class ProfileComponent {
-    
+
 }
