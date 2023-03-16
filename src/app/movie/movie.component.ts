5f0a6e4590298f7a4441723a2d3329437d9afd4f
Mickael Brunet <mickael.brunet64@gmail.com>
2023-03-16 11:02:04 +0100

    Add a route and a component for display a single movie

---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    standalone: true,
+    selector: 'tp-movies-movie',
+    templateUrl: 'movie.component.html',
+    styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent {
+
+}
