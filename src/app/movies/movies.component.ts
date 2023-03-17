ea6c5e6df32234dc88fc742a265e8a4a073cb6f1
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-03-17 12:10:30 +0100

    :sparkles: feat: create pagination component

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { PaginationComponent } from "../pagination/pagination.component";
 import { MovieCarouselComponent } from "../movie-carousel/movie-carousel.component";
 import { MoviesService } from "./movies.service";
 import { MovieContainerCommonComponent } from "../movie-container-common/movie-container-common.component";
@@ -18,7 +19,7 @@ interface genre {
     selector: "tp-movies-movies-api",
     templateUrl: "./movies.component.html",
     styleUrls: ['./movies.component.scss'],
-    imports: [CommonModule, HeaderComponent, FooterComponent, MovieCarouselComponent, MovieContainerCommonComponent]
+    imports: [CommonModule, HeaderComponent, FooterComponent, MovieCarouselComponent, MovieContainerCommonComponent, PaginationComponent]
 })
 export class MoviesComponent implements OnInit {
 
@@ -26,10 +27,16 @@ export class MoviesComponent implements OnInit {
     genre$ = this.apiService.getGenres();
     recentMovies: object[] | any;
     genres: genre[];
+    genreId: number;
+    currentPage: number;
+    totalPages: number;
 
     constructor(private readonly moviesService: MoviesService, private readonly apiService: apiService) {
         this.genres = [];
+        this.genreId = 0;
         this.recentMovies = [];
+        this.currentPage = 1;
+        this.totalPages = 5;
     }
 
     ngOnInit(): void {
@@ -46,12 +53,32 @@ export class MoviesComponent implements OnInit {
 
     getPopularMovies() {
         this.movies$.pipe(
-            tap(recentMovies => this.recentMovies = recentMovies)
+            tap(recentMovies => {
+                this.recentMovies = recentMovies
+                this.totalPages = recentMovies.total_pages > 500 ? 500 : recentMovies.total_pages
+            })
         ).subscribe()
     }
 
     filterGenre(genreId: number) {
+        this.genreId = genreId;
         this.apiService.getGenresMovies(genreId).pipe(tap(allGenreFilms => this.recentMovies = allGenreFilms)).subscribe();
-        console.log(this.recentMovies)
+    }
+
+    onPageChanged(e: number) {
+        this.currentPage = e
+        if (this.genreId == 0) {
+            this.apiService.getMovies(e).pipe(tap(allGenreFilms => {
+                this.recentMovies = allGenreFilms
+                this.totalPages = allGenreFilms.total_pages > 500 ? 500 : allGenreFilms.total_pages
+            }
+            )).subscribe();
+        }
+        else {
+            this.apiService.getGenresMovies(this.genreId, e).pipe(tap(allGenreFilms => {
+                this.recentMovies = allGenreFilms
+                this.totalPages = allGenreFilms.total_pages > 500 ? 500 : allGenreFilms.total_pages
+            })).subscribe();
+        }
     }
 }
