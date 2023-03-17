5dd66e0ddf31d51c8befbd3d07756e67c8f6f083
YohannTonnerre <yohann.tonnerre@gmail.com>
2023-03-17 16:18:01 +0100

    Revert ":sparkles: feat: search page"

--- src/api.service.ts
+++ src/api.service.ts
@@ -11,9 +11,6 @@ export class apiService {
     public getMovies(page: number = 1): Observable<any> {
         return this.httpClient.get(`https://api.themoviedb.org/3/movie/popular?api_key=81a8a2c4c129dd1c603073a9d974becb&language=en-US&page=${page}`)
     }
-    public getMoviesByTitle(query: string = '', page: number = 1): Observable<any> {
-        return this.httpClient.get(`https://api.themoviedb.org/3/search/movie?api_key=81a8a2c4c129dd1c603073a9d974becb&page=${page}&query=${query}`)
-    }
 
     public getGenres(): Observable<any> {
         return this.httpClient.get("https://api.themoviedb.org/3/genre/movie/list?api_key=81a8a2c4c129dd1c603073a9d974becb&language=en-US")
