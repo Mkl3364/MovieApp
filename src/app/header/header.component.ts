e7a51b03eaf30596ce4a4420b0e0a4250bce6409
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2023-03-23 14:28:10 +0100

    feat: flux activite

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,31 @@
 import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/compat/firestore";
+import { Movie } from "../movie/movie.service";
+import { Observable, tap } from "rxjs";
 
 
 @Component({
     standalone: true,
     selector: 'tp-movies-header',
     templateUrl: 'header.component.html',
-    styleUrls: ['header.component.scss']
+    styleUrls: ['header.component.scss'],
+    imports: [CommonModule]
 })
 export class HeaderComponent {
     
+    notificationIsOpen: boolean; 
+    private itemsCollection: AngularFirestoreCollection<Movie>;
+    items: Observable<Movie[]>;
+
+    constructor(private afs: AngularFirestore) {
+		this.notificationIsOpen = false;
+        this.itemsCollection = afs.collection<Movie>('movies');
+        this.items = this.itemsCollection.valueChanges();
+	}
+
+    notifications():void {
+        this.notificationIsOpen = !this.notificationIsOpen;
+        this.items.pipe(tap(test => console.log(test))).subscribe()
+    }
 }
