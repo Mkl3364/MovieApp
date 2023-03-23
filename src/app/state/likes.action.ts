986caf08c4dfd337028f39414a30001c429dbc2f
Mickael Brunet <mickael.brunet64@gmail.com>
2023-03-23 14:43:14 +0100

    remove like global state since it is store in user

--- src/app/state/likes.action.ts
+++
@@ -1,6 +0,0 @@
-export {}
-// import { createAction, props } from "@ngrx/store"
-// import { Movie } from "../movie/movie.service";
-
-// export const applicationStarted = createAction('[Startup] Application started');
-// export const movieLiked = createAction('Movie liked', props<{ movie: Movie }>());
