0e35b74616cca1631f9648a2a36523a60038ff89
Mickael Brunet <mickael.brunet64@gmail.com>
2023-03-29 15:43:40 +0200

    Account management with input value of email and pwd

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { Observable, tap } from "rxjs";
+import { UserInterface } from "../movie/movie.service";
+import { userLogSelector } from "../state/user.selector";
 
 
 @Component({
+    standalone: true,
     selector: "tp-movies-account",
     templateUrl: "account.component.html",
-    styleUrls: ['./account.component.scss']
+    styleUrls: ['./account.component.scss'],
+    imports: [RouterLink, RouterOutlet, CommonModule]
 })
 export class AccountComponent {
+    public user$: Observable<UserInterface> | undefined
+    public userEmail: string | undefined
+    public userPasword: string | undefined
 
+    constructor(private store: Store, public router: Router) {
+        this.user$ = this.store.select(userLogSelector)
+        this.user$.pipe(tap((account: UserInterface) => {
+            this.userEmail = account.email ?? ""
+        }))
+        this.user$.pipe(tap((account: UserInterface) => {
+            this.userPasword = account.uid
+        }))
+    }
+
+    onClickUserInput() {
+        this.router.navigateByUrl('/changeEmail')
+    }
 }
