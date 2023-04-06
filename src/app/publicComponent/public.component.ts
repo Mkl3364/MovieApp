ca34c2cafc73c08d2720ad8f32b525d438afed48
Mickael Brunet <mickael.brunet64@gmail.com>
2023-04-06 09:09:02 +0200

    Modify news feed page

--- src/app/publicComponent/public.component.ts
+++ src/app/publicComponent/public.component.ts
@@ -4,17 +4,24 @@ import { RouterLink } from "@angular/router";
 import { Observable } from "rxjs";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { CommonModule } from "@angular/common";
+import { PublicProfileContainerComponent } from "../public-profile-container/public-profile-container.component";
 @Component({
     standalone: true,
     selector: 'tp-movies-public',
     templateUrl: 'public.component.html',
     styleUrls: ['./public.component.scss'],
-    imports: [RouterLink, CommonModule]
+    imports: [RouterLink, CommonModule, PublicProfileContainerComponent]
 })
 export class PublicComponent implements OnInit {
     users$: Observable<any>
+    usersMovies$: Observable<any>
     constructor(private firestore: AngularFirestore, public auth: AngularFireAuth) {
         this.users$ = this.firestore.collection("user").valueChanges();
+        // this.auth.authState.subscribe((user) => {
+        //     this.usersMovies$ = this.firestore.doc(`movies/${user?.uid}`).get().pipe((res) => res);
+        //     console.log(this.firestore.doc(`movies/${user?.uid}`).get().pipe((res) => res))
+        // })
+        this.usersMovies$ = this.firestore.collection('movies').valueChanges()
     }
 
     ngOnInit(): void {
