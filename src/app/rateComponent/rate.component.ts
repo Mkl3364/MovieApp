e20a53765dfc823dba0f00b2bea319a6df7aa8b9
Mickael Brunet <mickael.brunet64@gmail.com>
2023-04-06 16:08:23 +0200

    Rated tab of public profile

--- src/app/rateComponent/rate.component.ts
+++ src/app/rateComponent/rate.component.ts
@@ -14,7 +14,9 @@ import { Observable } from "rxjs";
 export class RateComponent {
     moviesLiked$: Observable<any>
     public query: string;
+    public stars: number[];
     constructor(public firestore: AngularFirestore, public route: ActivatedRoute) {
+        this.stars = [1, 2, 3, 4, 5]
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         this.query = this.route.snapshot.paramMap.get('uid')!
         this.moviesLiked$ = this.firestore.doc(`movies/${this.query}`).valueChanges()
